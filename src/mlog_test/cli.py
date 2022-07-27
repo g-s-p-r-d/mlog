91d897033e454247866bc39e63656a94c051049f
Gaspard Lambrechts <gsprd@protonmail.com>
2022-07-27 16:16:22 +0200

    Add pull command and add scatter plots

--- src/mlog_test/cli.py
+++ src/mlog_test/cli.py
@@ -7,15 +7,25 @@ from argparse import ArgumentParser
 AGGREGATES = ['mean', 'median', 'min', 'max', 'std']
 
 
+def pull(args):
+    pass
+
+
 def plot(args):
     project = mlog.connect('project')
     df = project.get('_run_id', args.x_axis, args.y_axis)
 
+    if args.group and args.scatter:
+        raise NotImplementedError("Grouping not implemented for scatter plots")
+
     fig, ax = plt.subplots()
 
     if not args.group:
-        for run_id, run in df.groupby('_run_id'):
-            ax.plot(run[args.x_axis], run[args.y_axis])
+        if args.scatter:
+            ax.scatter(df[args.x_axis], df[args.y_axis])
+        else:
+            for run_id, run in df.groupby('_run_id'):
+                ax.plot(run[args.x_axis], run[args.y_axis])
 
     else:
         df = df.groupby(args.x_axis).aggregate(AGGREGATES)
@@ -30,27 +40,43 @@ def plot(args):
         ax.plot(df.index, df[args.y_axis][args.aggregate])
         ax.fill_between(df.index, df['_lower'], df['_upper'], alpha=0.4)
 
+    ax.set_xlabel(args.x_axis)
+    ax.set_ylabel(args.y_axis)
+
+    if args.output is not None:
+        plt.savefig(args.output)
+
     plt.show()
 
 
 def main():
-    # TODO: implement scatter plot
     # TODO: improve confidence intervals by defining aggregation functions
     # TODO: implement stderr confidence intervals
     # TODO: implement quantiles confidence intervals
 
     parser = ArgumentParser()
 
-    parser.add_argument('project')
-    parser.add_argument('x_axis')
-    parser.add_argument('y_axis')
+    subparser = parser.add_subparsers(required=True, dest='command')
+    parser_plot = subparser.add_parser('plot')
+    parser_pull = subparser.add_parser('pull')
 
-    parser.add_argument('-g', '--group', action='store_true')
-    parser.add_argument('-a', '--aggregate', choices=('mean', 'median'),
-                        default='mean')
-    parser.add_argument('-c', '--confidence', choices=('std', 'max'),
-                        default='std')
+    parser_plot.add_argument('project')
+    parser_plot.add_argument('x_axis')
+    parser_plot.add_argument('y_axis')
+
+    parser_plot.add_argument('-g', '--group', action='store_true')
+    parser_plot.add_argument('-a', '--aggregate', choices=('mean', 'median'),
+                             default='mean')
+    parser_plot.add_argument('-c', '--confidence', choices=('std', 'max'),
+                             default='std')
+
+    parser_plot.add_argument('-s', '--scatter', action='store_true')
+
+    parser_plot.add_argument('-o', '--output')
 
     args = parser.parse_args()
 
-    plot(args)
+    if args.command == 'plot':
+        plot(args)
+    elif args.command == 'pull':
+        pull(args)
